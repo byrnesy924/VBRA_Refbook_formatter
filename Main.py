a72c0a20aaef941781cc3797d4eb76bf126035aa
byrnesy924 <b.byrnesy@gmail.com>
2022-11-15 21:54:43 +1100

    V1.0 - fully functional and to be packaged into .exe

--- Main.py
+++ Main.py
@@ -8,18 +8,16 @@ from tkinter.messagebox import showinfo
 import time
 import pandas as pd
 import re
+from tabulate import tabulate
 
-ws = Tk()
-ws.title('PythonGuides')
-ws.geometry('1200x400')
-
+pd.set_option("display.max_columns", 8)
 
 def open_file():
     file_path = askopenfilename()
     if file_path is not None:
         pass
-    file_path_display.delete("1.0","end")
-    file_path_display.insert(END, file_path[0:3] + ".../" +  re.sub(".*\/([^\/]+)\/", "", file_path))
+    file_path_display.delete("1.0", "end")
+    file_path_display.insert(END, file_path[0:3] + ".../" + re.sub(".*\/([^\/]+)\/", "", file_path))
 
     # Handle file types
     global df, file_type
@@ -32,12 +30,11 @@ def open_file():
     else:
         messagebox.showerror('File Type Error', 'Error: file is not a csv or xlsx')
         return
-    print(df.head(5))
-    print(df.columns)
     csv_display.delete("1.0", "end")
-    csv_display_output.delete("1.0","end")
-    csv_display.insert(END, df.head(5))
+    csv_display_output.delete("1.0", "end")
+    csv_display.insert(END, df[["Competition", "Division", "Round", "Pay Class", "Venue", "Court"]].head(100).to_string(index=False))
     return df
+
 
 def competition_str_processor(string):
     if string[0:1] == 'U' and re.match("U18 BOYS", string):
@@ -48,44 +45,44 @@ def competition_str_processor(string):
         return "Saturday" + " " + re.sub("(\ BOYS).+|(\ GIRLS).+", "", string)
     return string.replace("/", "").title()
 
+
 def division_str_processor(string):
-    if re.match("(Division\ ).+", string) and not re.match("", string):
+    if re.match("(Division)", string):
         return ""
     return string.title()
 
+
 def remove_zero(string):
-    return string.replace("0", "").replace(" / ", "/")
+    return string.replace("0", "").replace(" / ", "/").replace("O'35", "O'35s")
+
+
+def reformat_O35s(string):
+    return string.replace("O'35", "O35's")
+
 
 def competition_str_mapper(string):
-    if re.match("(U8)|(U10)|(U12)|(U14)|(U16)", string):
+    if re.match("Saturday", string) or re.match("Tuesday U", string):
         return "Waverley Junior Domestic"
     return "Waverley Senior Domestic"
-
 
 
 def process_file(df):
     """This is a button function. It takes the csv and applies the changes. It then outputs it to the other text"""
     if not isinstance(df, pd.DataFrame):
         messagebox.showerror('Input Error', 'No file selected!')
-    # Do some stuff to the df TODO
     # Create court
-    df['Court_updated'] = df['Venue'] + df['Court'].apply(str).apply(lambda x: ' ' + x)
+    df['Court'] = df['Venue'] + df['Court'].apply(str).apply(lambda x: ' ' + x)
 
     # map venue
-    venue_dictionary = {'ORC':'Oakleigh Recreation Centre', 'WAV': 'Waverley Basketball Stadium',
-                        'ASB':'Ashburton Primary School'}
-    # TODO: Qn - are there any other venues?
-    df['Venue_updated'] = df['Venue'].apply(lambda x: venue_dictionary[x])
+    venue_dictionary = {'ORC': 'Oakleigh Recreation Centre', 'WAV': 'Waverley Basketball Stadium',
+                        'ASB': 'Ashburton Primary School'}
+    df['Venue'] = df['Venue'].apply(lambda x: venue_dictionary[x])
 
-    # TODO: Qn - what are the other pay classes?
     payclass_dictionary = {'Juniors': 'Juniors Standard', 'Seniors': 'Seniors Standard'}
-    df['Pay Class_updated'] = df['Pay Class'].apply(lambda x: payclass_dictionary[x])
+    df['Pay Class'] = df['Pay Class'].apply(lambda x: payclass_dictionary[x])
 
     # Update the Division column --> format the competition column, format the division column, add comp to div, map
-    # first apply this function to the "Competition" Column to get it to work correctly
-    # TODO: Qn - is Tuesday U16's meant to be Waverley Senior Domestic?
-
-    # iterrows - remove U18 division
+    # iterrows - remove U18 division from the division column - replace with empty string
     for index, row in df.iterrows():
         if row["Competition"][0:3] == "U18" or re.match("SUNDAY U/23", row["Competition"]):
             df.loc[index, "Division"] = ""
@@ -96,72 +93,81 @@ def process_file(df):
     # Apply - remove junior divisions
     df["Division"] = df["Division"].apply(lambda x: division_str_processor(x))
 
+    # Remove the slashes or zeros from the Division and add on the competition name
     df["Division"] = df["Competition"] + " " + df["Division"].apply(lambda x: remove_zero(x))
-    print(df[["Competition", "Division"]].iloc[160:190,:])
+    df["Division"] = df["Division"].apply(lambda x: reformat_O35s(x))
 
-    #TODO : fix this junior domestic crap
+    # Convert the Competition to just Junior or Senior Domestic
     df["Competition"] = df["Competition"].apply(lambda x: competition_str_mapper(x))
-    print(df[["Competition", "Division"]].iloc[160:190,:])
-
-
 
     csv_display_output.delete("1.0", "end")
-    csv_display_output.insert(END, df.tail())
+    csv_display_output.insert(END, df[["Competition", "Division", "Round", "Pay Class", "Venue", "Court"]]
+                              .head(100)
+                              .to_string(index=False))
+
 
 def save_file(df):
     save_file_path = asksaveasfilename()
-    print(save_file_path)
-    if file_type == "csv":
-        df.to_csv(str(save_file_path + ".csv"))
-    else:
-        df.to_excel(str(save_file_path + ".xlsx"))
+    df.to_excel(str(save_file_path + ".xlsx"))
+    # Assume refbook only takes xlsx --> functionality can be added later
+    # if file_type == "csv":
+    #     df.to_csv(str(save_file_path + ".csv"))
+    # else:
+    #     df.to_excel(str(save_file_path + ".xlsx"))
+    return
 
-addcsv = Label(
-    ws,
-    text='Upload CSV or Excel from WBA '
-)
-addcsv.grid(row=0, column=1, padx=10)
 
-addcsvbtn = Button(
-    ws,
-    text='Choose File',
-    command=lambda: open_file()
-)
-addcsvbtn.grid(row=0, column=2)
+if __name__ == "__main__":
+    ws = Tk()
+    ws.title('PythonGuides')
+    ws.geometry('1600x450')
 
-file_path_display = Text(ws, height=1, width=50)
-file_path_display.grid(row=0, column=3)
+    addcsv = Label(
+        ws,
+        text='Upload CSV or Excel from WBA '
+    )
+    addcsv.grid(row=0, column=1, padx=10)
 
-input_example = Label(
-    ws,
-    text='Input '
-)
-input_example.grid(row=3, column=1, padx=40)
+    addcsvbtn = Button(
+        ws,
+        text='Choose File',
+        command=lambda: open_file()
+    )
+    addcsvbtn.grid(row=0, column=2)
 
-output_example = Label(
-    ws,
-    text='Output '
-)
-output_example.grid(row=3, column=3, padx=40)
+    file_path_display = Text(ws, height=1, width=50)
+    file_path_display.grid(row=0, column=3)
 
-csv_display = Text(ws, height=9, width=50)
-csv_display.grid(row=4, column=1, padx=10)
+    input_example = Label(
+        ws,
+        text='Input '
+    )
+    input_example.grid(row=3, column=1, padx=40)
 
-csv_display_output = Text(ws, height=9, width=50)
-csv_display_output.grid(row=4, column=3, padx=10)
+    output_example = Label(
+        ws,
+        text='Output '
+    )
+    output_example.grid(row=3, column=3, padx=40)
 
-addcsvbtn = Button(
-    ws,
-    text='Process File',
-    command=lambda: process_file(df=df)
-)
-addcsvbtn.grid(row=6, column=1, padx=10, pady=10)
+    csv_display = Text(ws, height=20, width=70)
+    csv_display.grid(row=4, column=1, padx=10)
 
-save_file_btn = Button(
-    ws,
-    text='Save File',
-    command=lambda: save_file(df=df)
-)
-save_file_btn.grid(row=6, column=3, padx=10, pady=10)
+    csv_display_output = Text(ws, height=20, width=100)
+    csv_display_output.grid(row=4, column=3, padx=10)
 
-ws.mainloop()
+    addcsvbtn = Button(
+        ws,
+        text='Process File',
+        command=lambda: process_file(df=df)
+    )
+    addcsvbtn.grid(row=6, column=1, padx=10, pady=10)
+
+    save_file_btn = Button(
+        ws,
+        text='Save File',
+        command=lambda: save_file(df=df)
+    )
+    save_file_btn.grid(row=6, column=3, padx=10, pady=10)
+
+    ws.mainloop()
